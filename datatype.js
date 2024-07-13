099fc19ce03226d631d92bc9653b08ba87e91486
arpitparekh <arpitparekh9@gmail.com>
2024-07-13 12:48:23 +0530

    variables and datatype

---
+++ datatype.js
@@ -0,0 +1,16 @@
+let a = 12               // int
+let b = 12.34            // float
+let c = "Hello World"    // string
+
+let d = false        // boolean
+let e = true
+let f = null
+
+
+let g = undefined   // undefined
+g = 12
+g = "Helloi"
+
+g = 34.34
+
+console.log(g)
---
+++ variables.js
@@ -0,0 +1,34 @@
+// variable => variables are the names that we give to our data
+
+// varaible can be made using let keyword
+
+let a = 12
+
+let dsfs435f35mdskjfdshfs = 45
+let A = 45
+
+console.log(a)
+console.log(A)
+console.log(a + A)
+
+console.log("value of a is " + a)  // string and variavle concatination
+
+// let is keywords
+// keywords are some reserved words that we can use inside a syntax
+
+let b  // declaration
+
+b = 45  // variable initilization
+b = 44
+
+let c = 55  // declaration and initialization in same line
+c = 66   // re-initialization
+
+console.log("b is " + b)
+console.log("c is " + c)
+
+const h = 45   // we can not change the value of const keyword
+
+h = 67
+
+// let and const
