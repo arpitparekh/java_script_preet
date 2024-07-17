30053d815fd42bfbdfe667dd987f197d42c0d4a0
arpitparekh <arpitparekh9@gmail.com>
2024-07-17 12:44:07 +0530

    operators

--- datatype.js
+++ datatype.js
@@ -3,6 +3,7 @@ let b = 12.34            // float
 let c = "Hello World"    // string
 
 let d = false        // boolean
+
 let e = true
 let f = null
 
@@ -14,3 +15,7 @@ g = "Helloi"
 g = 34.34
 
 console.log(g)
+
+let h =34 
+console.log(h)
+console.log("sdzdsadsadsadad " + h)
---
+++ operators.js
@@ -0,0 +1,68 @@
+// operators are the symbols that we use in code
+
+// arithmetic operator
+
+// + - / * %  **
+
+let a = 10.34  // float
+let b = 5.67  // float
+
+console.log("Sum is : " + (a + b))
+console.log("Substraction is : " + (a - b))
+console.log("Division is : " + (a / b))
+console.log("Multiplication is : " + (a * b))
+
+let name1 = "Preet"   // str
+let number = 67
+
+console.log(name1 + number)
+
+// % = modulo
+
+console.log(10 % 12)
+
+console.log(678889 % 10)
+
+console.log(3**4)  // 
+
+// conditional operators  // will give us boolean value in return
+// < > <= >= == !=
+
+let x1 = 34
+let y1 = 45
+
+console.log(x1 < y1)
+
+console.log(x1 == y1)  // == equality operator
+console.log(x1 != y1)  // == equality operator
+
+// logical operators
+
+// && (and) || (or)
+// gives us boolean in return
+// between 2 conditional operator
+
+console.log(10 > 20 && 10 != 20)
+
+console.log(10 > 20 || 10 != 20)
+
+// increment  // decrement operators  // loops
+// ++  --
+
+let g = 45
+g--
+console.log(g)
+
+
+// assignment operator
+
+// =
+
+let h = 45
+h = 67
+
+//   +=   -=   *=  **=  /=  %=
+// h = h + 10
+h **= 10
+
+console.log(h)
