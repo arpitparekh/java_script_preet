a2a9ffaa0d8fff89da4944c1d3003530d3f3196c
arpitparekh <arpitparekh9@gmail.com>
2024-09-05 15:38:40 +0530

    date time

---
+++ javascript/date_class.js
@@ -0,0 +1,9 @@
+// date object
+
+let date = new Date();
+console.log(date);
+console.log(date.getDay());
+console.log(date.getFullYear());
+
+let date2 = new Date(2024, 1, 4);
+console.log(date2);
--- javascript/tempCodeRunnerFile.js
+++
@@ -1,2 +0,0 @@
-
-console.log(arr1.concat(arr2));  // joins the 2 array
